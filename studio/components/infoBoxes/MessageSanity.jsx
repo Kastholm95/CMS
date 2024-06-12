2d8e28688f210a0bdbcf0f3ddf8cc522d84b3279
Marc <marc@aguardio.com>
2024-06-12 15:32:36 +0200

    new msg

--- studio/components/infoBoxes/MessageSanity.jsx
+++ studio/components/infoBoxes/MessageSanity.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function MessageSanity() {
     return (
@@ -9,13 +9,21 @@ export default function MessageSanity() {
           borderRadius: '8px',
           margin: '20px',
           boxShadow: '0 2px 4px rgba(0,0,0,0.1)',
-          color: '#333', // Adding a default text color for better readability
-          fontFamily: 'Arial, sans-serif', // Ensuring text is easy to read with a common font
+          color: '#333', 
+          fontFamily: 'Arial, sans-serif', 
         }}>
-          <h3>Pause i Publicering</h3>
+          <h3>Nye Funktioner Implementeret </h3>
           <p>
-            Vent små 20 minutter med at publicere artikler, jeg laver lige  en stor opdatering. - Hilsen Marc
+            Jeg har indført flere automatiske opdateringer for at forbedre din oplevelse:
+          </p>
+          <ul>
+            <li><strong>Auto Oprettelse af Slugs:</strong> Slugs oprettes automatisk, når artikler publiceres første gang. Så i slipper for at klikke Generer</li>
+            <li><strong>Auto Tilknytning af Journalister:</strong> Journalisten tilknyttes automatisk baseret på den e-mail, der er logget ind med ved 1 Publish. Dette kan ændres manuelt nede i bunden, hvis nødvendigt.</li>
+            <li><strong>Profil Sikkerhed:</strong> Kun profiler, der er logget på med den samme e-mail , kan skrive artikler, hvilket sikrer, at kun autoriserede brugere kan bidrage.</li>
+          </ul>
+          <p>
+            Hvis i oplever problemer eller har spørgsmål, skriv/ring.
           </p>
         </div>
-      );
+    );
 }
