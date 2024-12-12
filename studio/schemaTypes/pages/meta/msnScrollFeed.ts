efdfd8b97360eaee414e2a1708c73ecc56601c7d
Marc <kastholm1995@gmail.com>
2024-12-12 15:59:32 +0100

    feed url display

--- studio/schemaTypes/pages/meta/msnScrollFeed.ts
+++ studio/schemaTypes/pages/meta/msnScrollFeed.ts
@@ -1,4 +1,7 @@
+import React from "react";
 import { validation } from "sanity";
+import { set, unset } from 'sanity'
+import { FeedUrlInput } from "../../../components/msnFeedUrl/FeedUrlInput";
 
 export default {
     name: 'msnScrollFeed',
@@ -24,24 +27,33 @@ export default {
             validation: (Rule: any) => Rule.required()
          },
          {
-              name: 'slug',
-              title: 'Generer et slug',
-              type: 'slug',
-              description: 'Klik på "Generate" for at generere et slug ud fra titlen - Dette bruges til at generere en URL til siden.',
-              options: {
-                   source: 'title',
-                   maxLength: 150,
-              },
-              hidden: true,
-         },
-         {
             name: 'articles',
             title: 'Tilføj artikler',
             type: 'array',
-            description: 'Tilføj 1 til 20 artikler der skal indgå i feedet. - Der kan kun tilvælges artikler som har en MSN feed beskrivelse',
+            description: 'Tilføj 4 til 20 artikler der skal indgå i feedet. - Der kan kun tilvælges artikler som har en MSN feed beskrivelse',
             of: [{ type: 'reference', to: [{ type: 'article'}], options:{filter: 'msnFeedDescription == true'}, _id: true }],
-            validation: (Rule: any) => Rule.required().min(1).max(20).error('Du kan vælge mellem 1 og 3 artikler'),
+            validation: (Rule: any) => Rule.required().min(4).max(20).error('Du skal vælge mellem 4 til 20 artikler'),
          },
+         {
+            name: 'slug',
+            title: 'Feed url - oprettes automatisk',
+            type: 'slug',
+            options: {
+              source: 'title',
+              maxLength: 150,
+            },
+            readOnly: true,
+            components: {
+              input: (props) => {
+                return React.createElement(
+                  'div',
+                  { style: { display: 'flex', alignItems: 'center' } },
+                  'https://pengehjoernet.dk/msnfeed/',
+                  props.renderDefault(props)
+                )
+              }
+            }
+          },          
         ],
     __experimental_omnisearch_visibility: false,
 }
